37ad0a9d72de26b601a4cc4d9c8cad01210bb4df
Francis Ferri <62342759+francis-ferri@users.noreply.github.com>
2020-09-14 14:50:26 -0500

    Login Adjusted

--- app/src/main/java/com/ferrifrancis/fixedbeams_phone/ui/LoadingActivity.kt
+++ app/src/main/java/com/ferrifrancis/fixedbeams_phone/ui/LoadingActivity.kt
@@ -1,10 +1,13 @@
 package com.ferrifrancis.fixedbeams_phone.ui
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.ferrifrancis.fixedbeams_phone.LOGIN_KEY
@@ -21,11 +24,21 @@ class LoadingActivity : AppCompatActivity() {
     private lateinit var  email: String
     private lateinit var password: String
 
+    private val CODIGO_SOLICITUD_PERMISO = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
         initializeSharedPreferences()
-        checkInternetStatus()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (validateLocationPermissions()){
+            checkInternetStatus()
+        } else {
+            askPermission()
+        }
     }
 
     private fun checkInternetStatus(){
@@ -37,12 +50,9 @@ class LoadingActivity : AppCompatActivity() {
                 goToLoginActivity()
             }
         } else {
-            Toast.makeText(this,"No Connection",Toast.LENGTH_LONG).show()
-            // TODO: Alert DIalog
+            createDialogNoConnection()
         }
     }
-
-    // TODO: Probar en on resume
 
 
     private fun checkSessionLogin(): Boolean{
@@ -52,7 +62,6 @@ class LoadingActivity : AppCompatActivity() {
         }
         return false
     }
-
 
     private fun goToMainActivity(){
         val intentToMain = Intent(this, MainActivity::class.java)
@@ -83,6 +92,51 @@ class LoadingActivity : AppCompatActivity() {
         email = sharedPreferences.getString(LOGIN_KEY, "").toString()
         password = sharedPreferences.getString(PASSWORD_KEY, "").toString()
     }
+
+    private fun createDialogNoConnection(){
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setTitle("No internet connection")
+        dialogBuilder.setMessage("You don't have internet connection...")
+        dialogBuilder.setIcon(R.drawable.ic_alert)
+        dialogBuilder.setPositiveButton("Retry") { _, _ -> checkInternetStatus() }
+        dialogBuilder.setNegativeButton("Quit") { _, _ -> finish() }
+        dialogBuilder.create().show()
+    }
+
+    private fun validateLocationPermissions(): Boolean{
+        val existsFineLocation: Boolean = ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        val existsCoarseLocation: Boolean = ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+       
+        return existsFineLocation && existsCoarseLocation
+    }
+
+    fun askPermission(){
+        requestPermissions(
+            arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION),
+            CODIGO_SOLICITUD_PERMISO)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            CODIGO_SOLICITUD_PERMISO -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    checkInternetStatus()
+                } else {
+                    finish()
+                }
+            }
+        }
+    }
+
 
 }
 
