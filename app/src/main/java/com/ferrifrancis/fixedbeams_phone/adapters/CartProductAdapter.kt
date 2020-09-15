9d34e0c8691dde921de7ea30f44d34a4510022a6
andr3slelouch <luis.andradec14@gmail.com>
2020-09-15 12:18:11 -0500

    Nueva implementacion con recyclerView

--- app/src/main/java/com/ferrifrancis/fixedbeams_phone/adapters/CartProductAdapter.kt
+++ app/src/main/java/com/ferrifrancis/fixedbeams_phone/adapters/CartProductAdapter.kt
@@ -1,68 +1,69 @@
-package com.ferrifrancis.fixedbeams_phone.ui.adapter
-
-import android.app.Activity
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.ferrifrancis.fixedbeams_phone.R
 import com.ferrifrancis.fixedbeams_phone.data.product.ProductModelClass
 import com.ferrifrancis.fixedbeams_phone.util.SharedPreferencesManager
-import com.squareup.picasso.Callback
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.layout_shopping_cart_list_item.view.*
 
-class CartProductAdapter (private val context: Activity, private val products: ArrayList<ProductModelClass>): BaseAdapter(){
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val inflater = context.layoutInflater
-        val rowView = inflater.inflate(R.layout.layout_shopping_cart_list_item, null, true)
-        val textViewProductName = rowView.findViewById<TextView>(R.id.textView_productName)
-        val textViewProductPrice = rowView.findViewById<TextView>(R.id.textView_price)
-        val textViewDiscount = rowView.findViewById<TextView>(R.id.textView_discount)
-        val imageViewProduct = rowView.findViewById<ImageView>(R.id.imageViewProductImage)
-        val textViewQuantity = rowView.findViewById<TextView>(R.id.textView_quantity)
 
-        Glide.with(context).load(products[position].srcImage)
-            .centerInside()
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(imageViewProduct)
-
-        textViewProductName.text = "${products[position].name}"
-        textViewProductPrice.text = "$ ${products[position].price}"
-        textViewQuantity.text = products[position].quantity.toString()
-        textViewDiscount.text = "Discount: 0%"
-        addButtonListeners(rowView, position)
-        return rowView
+class CartProductAdapter(
+    val products: ArrayList<ProductModelClass>
+): RecyclerView.Adapter<CartProductAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CartProductAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.layout_shopping_cart_list_item, parent, false)
+        return ViewHolder(v)
     }
 
-    fun addButtonListeners(itemView: View, position: Int){
-        var tempCounter = itemView.textView_quantity.text.toString().toInt()
-        itemView.plus_button.setOnClickListener {
-            tempCounter += 1
-            itemView.textView_quantity.text = tempCounter.toString()
-            products[position].quantity = tempCounter
-            SharedPreferencesManager.saveProduct(products[position], itemView.context, "CarritoAdaptador")
-        }
-        itemView.minus_button.setOnClickListener {
-            tempCounter -= 1
-            itemView.textView_quantity.text = tempCounter.toString()
-            products[position].quantity = tempCounter
-            SharedPreferencesManager.saveProduct(products[position], itemView.context, "CarritoAdaptador")
-        }
+    override fun onBindViewHolder(holder: CartProductAdapter.ViewHolder, position: Int) {
+        holder.bindItems(products[position])
     }
 
-    override fun getItem(position: Int): Any {
-        return products.get(position)
-    }
-
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
-
-    override fun getCount(): Int {
+    override fun getItemCount(): Int {
         return products.size
     }
 
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bindItems(product: ProductModelClass){
+            val textViewProductName = itemView.findViewById<TextView>(R.id.textView_productName)
+            val textViewProductPrice = itemView.findViewById<TextView>(R.id.textView_price)
+            val textViewDiscount = itemView.findViewById<TextView>(R.id.textView_discount)
+            val imageViewProduct = itemView.findViewById<ImageView>(R.id.imageViewProductImage)
+            val textViewQuantity = itemView.findViewById<TextView>(R.id.textView_quantity)
+
+            Glide.with(itemView).load(product.srcImage)
+                .centerInside()
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(imageViewProduct)
+
+            textViewProductName.text = "${product.name}"
+            textViewProductPrice.text = "$ ${product.price}"
+            textViewQuantity.text = product.quantity.toString()
+            textViewDiscount.text = "Discount: 0%"
+
+        }
+        fun addButtonListeners(itemView: View, product: ProductModelClass){
+            var tempCounter = itemView.findViewById<TextView>(R.id.textView_quantity).text.toString().toInt()
+            itemView.plus_button.setOnClickListener {
+                tempCounter += 1
+                itemView.textView_quantity.text = tempCounter.toString()
+                product.quantity = tempCounter
+                SharedPreferencesManager.saveProduct(product, itemView.context, "CarritoAdaptador")
+            }
+            itemView.minus_button.setOnClickListener {
+                tempCounter -= 1
+                itemView.textView_quantity.text = tempCounter.toString()
+                product.quantity = tempCounter
+                SharedPreferencesManager.saveProduct(product, itemView.context, "CarritoAdaptador")
+            }
+        }
+    }
 }
--- app/src/main/java/com/ferrifrancis/fixedbeams_phone/ui/ShoppingCartActivity.kt
+++ app/src/main/java/com/ferrifrancis/fixedbeams_phone/ui/ShoppingCartActivity.kt
@@ -1,21 +1,26 @@
 package com.ferrifrancis.fixedbeams_phone.ui
 
+import CartProductAdapter
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ferrifrancis.fixedbeams_phone.R
 import com.ferrifrancis.fixedbeams_phone.data.product.ProductModelClass
-import com.ferrifrancis.fixedbeams_phone.ui.adapter.CartProductAdapter
 import com.ferrifrancis.fixedbeams_phone.util.SharedPreferencesManager
 import kotlinx.android.synthetic.main.activity_products.*
 import kotlinx.android.synthetic.main.activity_shopping_cart.*
 
 class ShoppingCartActivity : AppCompatActivity() {
     var products = arrayListOf<ProductModelClass>()
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_cart)
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView_cart_products)
+        recyclerView.layoutManager = LinearLayoutManager(this)
         obtainProducts()
         continue_button.setOnClickListener {
             goToPagosActivity()
@@ -26,11 +31,10 @@ class ShoppingCartActivity : AppCompatActivity() {
         if(products.size == 0){
             Toast.makeText(this, "No existen productos", Toast.LENGTH_LONG).show()
         }
-        listView_cart_products.adapter =
-            CartProductAdapter(
-                this,
-                products
-            )
+        val adapter = CartProductAdapter(products)
+
+        recyclerView.adapter = adapter
+
     }
     fun goToPagosActivity(){
         val intentExplicito = Intent(this,
